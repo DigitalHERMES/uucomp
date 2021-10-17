2ab11f558d7298988421101ed565f59bba648c4e
Rafael Diniz <rafael@riseup.net>
2021-10-17 20:15:49 +0300

    new generation compress image

--- scripts/compress_image.sh
+++ scripts/compress_image.sh
@@ -11,6 +11,8 @@
 # initial VVC QP... it will only get bigger...
 VVC_QP=39
 
+MAX_DIMENSION_SIZE=840
+
 QUALITY=75 # initial start quality to try for jpeg
 
 VVC_ENC=${VVC_ENC:=/opt/vvc/vvencapp}
@@ -43,67 +45,83 @@ IMAGE_FORMAT="${output_file##*.}"
 TEMPFILE=/tmp/temp-$$.${IMAGE_FORMAT}
 TEMPFILEYUV=/tmp/temp-$$.yuv
 
-echo "Original file size = $(stat -c%s "${input_file}")"
+# this was for JPEG support...
+## cp -f "${input_file}" ${TEMPFILE}
 
-# while [[ stat -c ${input_file} ]]
-# echo $(stat -c%s "${input_file}")
-cp -f "${input_file}" ${TEMPFILE}
+resolution=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=p=0 "${input_file}" | sed 's/,/x/g')
+width=$(echo -n ${resolution} | cut -f 1 -d x)
+height=$(echo -n ${resolution} | cut -f 2 -d x)
+
+changed_resolution=0
+
+echo "Original File size = $(stat -c%s "${input_file}")"
+echo "Original Resolution = ${resolution}"
+
+if [ "${width}" -gt "${MAX_DIMENSION_SIZE}" ] || [ "${height}" -gt "${MAX_DIMENSION_SIZE}" ]; then
+  if [ "${width}" -gt "${height}" ]; then
+    new_width=${MAX_DIMENSION_SIZE}
+    ratio=$(awk "BEGIN {x=${width}/${new_width}; printf \"%.4f\n\",x}")
+    new_height=$(awk "BEGIN {x=${height}/${ratio}; printf \"%d\",x}")
+  else
+    new_height=${MAX_DIMENSION_SIZE}
+    ratio=$(awk "BEGIN {x=${height}/${new_height}; printf \"%.4f\n\",x}")
+    new_width=$(awk "BEGIN {x=${width}/${ratio}; printf \"%d\",x}")
+  fi
+  width=${new_width}
+  height=${new_height}
+  changed_resolution=1
+fi
+
+# make resolution multiple of 8
+width=$(( (${width} / 8) * 8 ))
+height=$(( (${height} / 8) * 8 ))
+resolution=${width}x${height}
+
+echo "New Resolution = ${resolution}"
+
+if [ ${changed_resolution} -eq "1" ]; then
+  echo "Content will be downscaled"
+fi
+
+
+# ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nw=1:nk=1
+# ffmpeg -i ../../testeCapacidadeMidias/images/FotoFoneMaq_Zap_IncendioBernardoSayao10-07-21_Felipe_Spina_Avino_WWF\ \(1\).jpeg -vf scale=840:840:force_original_aspect_ratio=decrease output.jpg
+# ffmpeg -i blue.png -vf scale=out_color_matrix=bt709:flags=full_chroma_int+accurate_rnd,format=yuv420p yuv420p_709.yuv
+# -vf scale=840:840:force_original_aspect_ratio=decrease
+# convert directly to: (in order to lose less info...)
+# -pix_fmt yuv420p10le
+
+ffmpeg -y -i "${input_file}"  -f rawvideo -vf scale=width=${width}:height=${height}:out_color_matrix=bt709,format=yuv420p10le ${TEMPFILEYUV}
 
 if [ ${IMAGE_FORMAT} = "evc" ]; then
-    resolution=$(convert-im6 -debug all -resize "840x840>" "${input_file}" -sampling-factor 4:2:0 -depth 8 -colorspace Rec709YCbCr ${TEMPFILEYUV} 2>&1 | grep -i "Heap " | cut -d " " -f 7 | sed -n 5p)
-    width=$(echo -n ${resolution} | cut -f 1 -d x)
-    height=$(echo -n ${resolution} | cut -f 2 -d x)
 
-    # my ugly workaround to round up the dimensions
-    width=$(( (${width} / 8) * 8 ))
-    height=$(( (${height} / 8) * 8 ))
-    resolution=${width}x${height}
-
-    rm -f ${TEMPFILEYUV}
-
-    convert-im6 -resize "${resolution}!" "${input_file}" -sampling-factor 4:2:0 -depth 8 -colorspace Rec709YCbCr ${TEMPFILEYUV}
-
-
-    echo ${EVC_ENC} -w ${width} -h ${height} -z 1 -m 2 --profile main --preset medium --bitrate $(( ${TARGET_SIZE} / 1000 ))  -i ${TEMPFILEYUV} -o  ${TEMPFILE}
-    ${EVC_ENC} -w ${width} -h ${height} -z 1 -m 2 --profile main --preset medium --bitrate $(( ${TARGET_SIZE} / 1000 ))  -i ${TEMPFILEYUV} -o  ${TEMPFILE}
-
-    rm -f ${TEMPFILEYUV}
+    echo ${EVC_ENC} -w ${width} -h ${height} -d 10 -z 1 -m 2 --profile main --preset medium --bitrate $(( ${TARGET_SIZE} / 1000 ))  -i ${TEMPFILEYUV} -o  ${TEMPFILE}
+    ${EVC_ENC} -w ${width} -h ${height} -d 10  -z 1 -m 2 --profile main --preset medium --bitrate $(( ${TARGET_SIZE} / 1000 ))  -i ${TEMPFILEYUV} -o  ${TEMPFILE}
 
 elif [ ${IMAGE_FORMAT} = "vvc" ]; then
-
-    #    ffmpeg -i ${input_file} -c:v rawvideo -pixel_format yuv420p -vf scale=-1:840  output_720x480p.yuv
-    resolution=$(convert-im6 -debug all -resize "840x840>" "${input_file}" -sampling-factor 4:2:0 -depth 8 -colorspace Rec709YCbCr ${TEMPFILEYUV} 2>&1 | grep -i "Heap " | cut -d " " -f 7 | sed -n 5p)
-    width=$(echo -n ${resolution} | cut -f 1 -d x)
-    height=$(echo -n ${resolution} | cut -f 2 -d x)
-
-    # my ugly workaround to round up the dimensions
-    width=$(( (${width} / 4) * 4 ))
-    height=$(( (${height} / 4) * 4 ))
-    resolution=${width}x${height}
-
-    rm -f ${TEMPFILEYUV}
-
-    convert-im6 -resize "${resolution}!" "${input_file}" -sampling-factor 4:2:0 -depth 8 -colorspace Rec709YCbCr ${TEMPFILEYUV}
 
     ## bitrate control using rc
     #   ${VVC_ENC} -i ${TEMPFILEYUV} --profile main_10_still_picture --qpa 1 -t 2 -r 1 -b ${TARGET_SIZE} -s ${resolution} --preset medium -c yuv420 -o  ${TEMPFILE}
 
-    ${VVC_ENC} -i ${TEMPFILEYUV} --profile main_10_still_picture --qpa 1 -t 2 -r 1 --qp ${VVC_QP} -s ${resolution} --preset medium -c yuv420 -o  ${TEMPFILE}
+    ${VVC_ENC} -i ${TEMPFILEYUV} --profile main_10_still_picture --qpa 1 -c yuv420_10 -t 2 -r 1 --qp ${VVC_QP} -s ${resolution} --preset medium -c yuv420 -o  ${TEMPFILE}
 
     while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$VVC_QP" -lt "61" ]; do
       VVC_QP=$((VVC_QP+3))
-      ${VVC_ENC} -i ${TEMPFILEYUV} --profile main_10_still_picture --qpa 1 -t 2 -r 1 --qp ${VVC_QP} -s ${resolution} --preset medium -c yuv420 -o  ${TEMPFILE}
+      ${VVC_ENC} -i ${TEMPFILEYUV} --profile main_10_still_picture --qpa 1 -c yuv420_10 -t 2 -r 1 --qp ${VVC_QP} -s ${resolution} --preset medium -c yuv420 -o  ${TEMPFILE}
 
     done;
 
     rm -f ${TEMPFILEYUV}
 
 elif [ ${IMAGE_FORMAT} = "avif" ]; then
-
+  echo "AVIF support not implemented."
+  exit
 # TODO
-  ${AV1_ENC} --target-bitrate=${TARGET_SIZE} --end-usage=cbr --bit-depth=8 ...
-
+##  ${AV1_ENC} --target-bitrate=${TARGET_SIZE} --end-usage=cbr --bit-depth=8 ...
 elif [ ${IMAGE_FORMAT} = "jpg" ]; then
+
+  echo "JPEG support is temporarily removed."
+  exit
 
   while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "${QUALITY}" -gt "5" ]; do
     convert -resize "840x840>" "${input_file}" pnm:- | /opt/mozjpeg/bin/cjpeg -quality ${QUALITY} > ${TEMPFILE}
@@ -112,11 +130,14 @@ elif [ ${IMAGE_FORMAT} = "jpg" ]; then
 
 elif [ ${IMAGE_FORMAT} = "heic" ]; then
 
+  echo "HEIF support not implemented."
+  exit
+
 # TODO
-  while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
-    convert -resize "840x840>"  "${input_file}" -quality ${QUALITY} ${TEMPFILE}
-    QUALITY=$((QUALITY-10))
-  done;
+##  while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
+##    convert -resize "840x840>"  "${input_file}" -quality ${QUALITY} ${TEMPFILE}
+##    QUALITY=$((QUALITY-10))
+##  done;
 
 else
   echo "Unsupported extension: ${IMAGE_FORMAT}."
@@ -127,6 +148,8 @@ fi
 #if [ $# -eq 1 ]; then
 #  mv ${TEMPFILE} "${input_file}"
 #fi
+
+rm -f ${TEMPFILEYUV}
 
 echo "Final file size: $(stat -c%s "${TEMPFILE}")"
 
