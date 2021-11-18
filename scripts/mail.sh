887870cbb06cab4d633febdb292ef8cafcdcbb9b
Carlos Paulino <chgp@riseup.net>
2021-11-18 17:06:23 -0300

    mail.sh example on how to get mail info from a uucp pack and return mail

---
+++ scripts/mail.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+file=${BASH_ARGV[*]}
+#sample
+#file="/var/spool/uucp/B0DE/C./C.NRIPLjRAA5vU"
+echo "file: "  $file
+
+#filter crmail from C  and get D
+D=$(cat $file | grep crmail | awk '{print $6 ;}' )
+echo "D: " $D
+
+#get dest 
+to=$(cat $file | cut -d ' ' -f 11- )
+echo "To: " $to
+
+#get uucp host path prefix
+#file_dir=$(echo $file | awk -F '/' '{print $1"/";}')
+
+host_dir=$(echo $file | awk -F '/' '{print $5;}')
+echo "host_dir " $host_dir
+
+from=$(zcat /var/spool/uucp/$host_dir/D./$D | head -n1 | awk '{print $2;}')
+echo "from " $from
+
+subject=$(zcat /var/spool/uucp/$host_dir/D./$D | head -n20 |grep Subject| awk '{print $2;}')
+echo "subject " $subject
+
+#send mail
+echo "seu email com titulo: $subject \n enviado para $to foi limado "| mail $from 
