0493d9e0f0a9bc82a77633cc79bdae8821c07288
Rafael Diniz <rafael@riseup.net>
2021-11-19 12:55:00 +0300

    scheleton to delete newest emails when queue size is above limit

--- scripts/mail_manager.sh
+++ scripts/mail_manager.sh
@@ -37,14 +37,36 @@ do
   fi
 done
 
+
+# TODO: use a: WHILE  [ ${total_size} -gt ${MAX_EMAIL_SIZE} ] ....
 # now check for the size of the full uucp queue... and delete the newest emails..
 total_size=$(uustat -a | awk -F ' ' '{sum+=$(NF - 1);}END{print sum;}')
 
 if [ ${total_size} -gt ${MAX_EMAIL_SIZE} ]
 then
-  echo "Total size of the UUCP (${total_size}) exceed the maximum of ${MAX_EMAIL_SIZE}"
-  # iterate in all emails...
-  # use the SEQF? # use  the time of creation of the C. to identify the latest created email?
+  echo "Total size of the UUCP (${total_size}) exceed the maximum of ${MAX_EMAIL_SIZE}. Deleting last email."
+
+  for i in $(ls -1 /var/spool/uucp/)
+  do
+    if [ -d /var/spool/uucp/${i}/C. ]
+    then
+      echo "Looking at system ${i}"
+      cd /var/spool/uucp/${i}/C.
+      for j in $(ls -1)
+      do
+        # check if it is mail
+        cmd=$(cat ${j} | cut -d ' '  -f 10 )
+        if [ "${cmd}" == "crmail" ]
+        then
+          # get the stat in unix seconds timestamp pra ficar facil de achar quem eh o mais novo...
+        fi
+
+      done
+    fi
+  done
+
+  # delete the newest email...
+
 else
   echo "Total UUCP queue size ${total_size} is good"
 fi
