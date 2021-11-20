a4264313c4e0e4c99d3914be4dc7e64937b81bcd
Carlos Paulino <chgp@riseup.net>
2021-11-19 21:18:36 -0300

    handle language and type of message on mailkill

--- scripts/mailkill.sh
+++ scripts/mailkill.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-  echo "need uuid arg"
-  exit
+if [ $# -eq 0 ] || [ $# -gt 3 ] || [ $# -eq 2 ]; then
+  echo "Usage: $0 [es|en sys|gui] uuid"
+  exit 1
 fi
 
-#get uuid from args
-uuid=${BASH_ARGV[*]}
+#get args
+if [ $# -eq 3 ]; then
+  lang=${1}
+  type=${2}
+  uuid=${3}
+elif [ $# -eq 1 ]; then
+  lang="en"
+  type="sys"
+  uuid=$1
+fi
 
 #get uucp host path prefix
 host=$(echo $uuid | cut -d "." -f1)
@@ -40,6 +48,19 @@ subject=$(zcat $fullD | head -n20 |grep Subject| awk '{print $2;}')
 
 kill=$(uustat -k $uuid)
 
-#send email
-echo "Your email with title: $subject \n to $to was cancelled"| mail  -s "email cancelled" $from
+if [ $lang = "en" ] && [ $type = "gui" ]; then
+  message="Your email with destination(s): $to with subject: $subject was canceled by the admin user. \n\n This is an automatic message from Hermes System!"
+  subject="Email canceled by the admin user"
+elif [ $lang = "en" ] && [ $type = "sys" ]; then
+  message="Your email with destination(s): $to with subject: $subject was canceled because exceeds system limits. \n\n This is an automatic message from Hermes System!"
+  subject="Email canceled by the system"
+elif [ $lang = "es" ] && [ $type = "gui" ]; then
+  message="Su correo electrónico con destino(s): $to con asunto: $subject fue cancelado por el usuario administrador. \n\n Este es un mensaje del sistema automático de Hermes!"
+  subject="Email cancelado por el administrador"
+elif [ $lang = "es" ] && [ $type = "sys" ]; then
+  message="Su correo electrónico con destino(s): $to con asunto: $subject fue cancelado por exceder los límites del sistema. \n\n Este es un mensaje del sistema automático de Hermes!"
+  subject="Email cancelado por el sistema"
+fi
 
+#send email
+echo $message | mail  -s "email cancelled" $from
