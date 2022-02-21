69db4703fe51457bf507a9df8b1247be3fa8538a
Rafael Diniz <rafael@riseup.net>
2022-02-21 12:29:06 +0300

    fix last big race conditions...

--- scripts/mail_manager.sh
+++ scripts/mail_manager.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Usage mail_manager.sh [uucp C. file]
+
 
 # maximum email size
 MAX_EMAIL_SIZE=${MAX_EMAIL_SIZE:=20000}
@@ -31,37 +33,65 @@ get_uucp_queue_size_for_host ()
   fi
 }
 
-for i in $(ls -1 /var/spool/uucp/)
-do
-  if [ -d /var/spool/uucp/${i}/C. ]
-  then
-    echo "Looking at system ${i}"
-    cd /var/spool/uucp/${i}/C.
-    for j in $(ls -1)
-    do
-      uucomp ${j}
+# if the uucp C. is not specified in argv[1]
+if [ -z "${1}" ]; then
+  for i in $(ls -1 /var/spool/uucp/)
+  do
+    if [ -d /var/spool/uucp/${i}/C. ]
+    then
+      echo "Looking at system ${i}"
+      cd /var/spool/uucp/${i}/C.
+      for j in $(ls -1)
+      do
+        uucomp ${j}
 
-      # check if it is mail
-      cmd=$(cat ${j} | cut -d ' '  -f 10 )
-      if [ "${cmd}" == "crmail" ]
-      then
-        D=$(cat ${j} | grep crmail | awk '{print $2 ;}')
-        D_size=$(stat -c %s ../D./${D})
-        id=$(echo ${j} | cut -d . -f 2)
-        uuid="${i}.${id}"
-        if [ ${D_size} -gt ${MAX_EMAIL_SIZE} ]
+        # check if it is mail
+        cmd=$(cat ${j} | cut -d ' '  -f 10 )
+        if [ "${cmd}" == "crmail" ]
         then
-          echo "UUID = ${uuid} SIZE ${D_size} WILL ME MAILKILLED!"
-          mailkill.sh size_limit ${uuid}
-        else
-          echo "UUID = ${uuid} SIZE ${uuid} IS GOOD"
+          D=$(cat ${j} | grep crmail | awk '{print $2 ;}')
+          D_size=$(stat -c %s ../D./${D})
+          id=$(echo ${j} | cut -d . -f 2)
+          uuid="${i}.${id}"
+          if [ ${D_size} -gt ${MAX_EMAIL_SIZE} ]
+          then
+            echo "UUID = ${uuid} SIZE ${D_size} WILL ME MAILKILLED!"
+            mailkill.sh size_limit ${uuid}
+          else
+            echo "UUID = ${uuid} SIZE ${uuid} IS GOOD"
+          fi
         fi
-      fi
 
-    done
+      done
+    fi
+  done
+
+  # parse the argv[1]
+else
+  host=$(echo ${1}  | rev | cut -d '/' -f 3 | rev)
+  C=$(echo ${1}  | rev | cut -d '/' -f 1 | rev)
+  echo "Looking at system ${host}"
+  cd /var/spool/uucp/${host}/C.
+
+  uucomp ${C}
+
+  cmd=$(cat ${C} | cut -d ' '  -f 10 )
+  if [ "${cmd}" == "crmail" ]
+  then
+    D=$(cat ${C} | grep crmail | awk '{print $2 ;}')
+    D_size=$(stat -c %s ../D./${D})
+    id=$(echo ${C} | cut -d . -f 2)
+    uuid="${host}.${id}"
+    if [ ${D_size} -gt ${MAX_EMAIL_SIZE} ]
+    then
+      echo "UUID = ${uuid} SIZE ${D_size} WILL ME MAILKILLED!"
+      mailkill.sh size_limit ${uuid}
+    else
+      echo "UUID = ${uuid} SIZE ${uuid} IS GOOD"
+    fi
   fi
-done
 
+fi
 
 # now check for the size of the emails in the uucp queue for each host... 
 # and delete the newest emails.. but not rmail, as there might
