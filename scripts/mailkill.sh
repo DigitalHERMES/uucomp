1b9696bd6c91c190308f34e3b7bba253947e9966
Rafael Diniz <rafael@riseup.net>
2022-05-26 10:09:41 -0300

    added pt translation to mailkill.sh

--- scripts/mailkill.sh
+++ scripts/mailkill.sh
@@ -51,26 +51,36 @@ local_time="$(date -d @$(stat -c %W ${fullC}) '+%H:%M %d/%m/%Y' )"
 kill=$(uustat -k $uuid)
 
 if [ $lang = "en" ] && [ $type = "gui" ]; then
-  message="Your email with destination(s): $to sent at ${local_time} was canceled by the admin user. \n\nThis is an automatic message from Hermes System!"
+  message="Your email with destination(s): ${to} sent at ${local_time} was canceled by the admin user. \n\nThis is an automatic message from HERMES System!"
   subject="Email canceled by the admin user"
 elif [ $lang = "en" ] && [ $type = "size_limit" ]; then
-  message="Your email with destination(s): $to sent at ${local_time} was canceled because exceeds maximum email size limit. \n\nThis is an automatic message from Hermes System!"
+  message="Your email with destination(s): ${to} sent at ${local_time} was canceled because exceeds maximum email size limit. \n\nThis is an automatic message from HERMES System!"
   subject="Email canceled by the system"
 elif [ $lang = "en" ] && [ $type = "queue_full" ]; then
-  message="Your email with destination(s): $to sent at ${local_time} was canceled because transmission list exceeds maximum size. \n\nThis is an automatic message from Hermes System!"
+  message="Your email with destination(s): ${to} sent at ${local_time} was canceled because transmission list exceeds maximum size. \n\nThis is an automatic message from HERMES System!"
   subject="Email canceled by the system"
 
 elif [ $lang = "es" ] && [ $type = "gui" ]; then
-  message="Su correo electrónico con destino(s): $to enviado el ${local_time} fue cancelado por el usuario administrador. \n\nEste es un mensaje del sistema automático de Hermes!"
+  message="Su correo electrónico con destino(s): ${to} enviado el ${local_time} fue cancelado por el usuario administrador. \n\nEste es un mensaje del sistema automático de HERMES!"
   subject="Email cancelado por el administrador"
 elif [ $lang = "es" ] && [ $type = "size_limit" ]; then
-  message="Su correo electrónico con destino(s): $to enviado el ${local_time} fue cancelado porque supera el límite máximo de tamaño de correo electrónico. \n\nEste es un mensaje del sistema automático de Hermes!"
+  message="Su correo electrónico con destino(s): ${to} enviado el ${local_time} fue cancelado porque supera el límite máximo de tamaño de correo electrónico. \n\nEste es un mensaje del sistema automático de HERMES!"
   subject="Email cancelado por el sistema"
 elif [ $lang = "es" ] && [ $type = "queue_full" ]; then
-  message="Su correo electrónico con destino(s): $to enviado el ${local_time} fue cancelado porque la lista de transmisión excede el tamaño máximo. \n\nEste es un mensaje del sistema automático de Hermes!"
+  message="Su correo electrónico con destino(s): ${to} enviado el ${local_time} fue cancelado porque la lista de transmisión excede el tamaño máximo. \n\nEste es un mensaje del sistema automático de HERMES!"
   subject="Email cancelado por el sistema"
+
+if [ $lang = "pt" ] && [ $type = "gui" ]; then
+  message="Seu e-mail com destino(s): ${to} enviado em ${local_time} foi cancelado pelo usuário administrador. \n\nEsta é uma mensagem automática do Sistema HERMES!"
+  subject="Email cancelado pelo usuário administrador"
+elif [ $lang = "pt" ] && [ $type = "size_limit" ]; then
+  message="Seu e-mail com destino(s): ${to} enviado em ${local_time} foi cancelado porque excede o limite máximo de tamanho de e-mail. \n\nEsta é uma mensagem automática do Sistema HERMES!"
+  subject="E-mail cancelado pelo sistema"
+elif [ $lang = "pt" ] && [ $type = "queue_full" ]; then
+  message="Seu e-mail com destino(s): ${to} enviado em ${local_time} foi cancelado porque a lista de transmissão excede o tamanho máximo. \n\nEsta é uma mensagem automática do Sistema HERMES!"
+  subject="E-mail cancelado pelo sistema"
 
 fi
 
 #send email
-echo -e $message | mail  -s "email cancelled" $from
+echo -e ${message} | mail  -s ${subject} $from
